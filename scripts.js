90161d8af0ce19b8c7969e8b72ec9b3c67c88b91
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-10-24 12:55:24 -0300

    Iniciando funções básicas js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// Definindo um número aleátório até 50
+const number = Math.trunc(Math.random() * 50 + 1);
+console.log(number);
+
+// Verifica número
+const checkNumber = function() {
+    let guess = Number(document.querySelector(".guess").value);
+    if (!guess) {
+        console.log("Número inválido!");
+    } else {
+        console.log(guess);
+        document.querySelector(".guess").value = "";
+    }
+}
+
+// Ao clicar no botão "Check" ele chama a função verificar o número
+document.querySelector(".check").addEventListener("click", checkNumber);
