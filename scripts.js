13de4e54f81f17f979d983ed2c80b97f8caccce9
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-10-24 23:55:16 -0300

    Adicionado fn novo jogo e algumas correções no js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,49 @@
 'use strict';
 
-// Definindo um número aleátório até 50
-const number = Math.trunc(Math.random() * 50 + 1);
-console.log(number);
+// Define até qual N° vai o jogo
+const numMax = 20;
+let score = numMax;
+let highscore = 0;
+
+// Define o N° aleatório secreto
+let secretNumber = Math.trunc(Math.random() * numMax + 1);
+
+// Define os valores no DOM
+document.querySelector(".score").textContent = score;
+document.querySelector(".between").textContent = `(Escolha um n° entre 1 e ${numMax})`;
+
+const newGame = function() {
+    let score = numMax;
+    let secretNumber = Math.trunc(Math.random() * numMax + 1);
+    document.querySelector(".guess").value = "";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".message").textContent = "Tente acertar qual o número!";
+}
 
 // Verifica número
 const checkNumber = function() {
     let guess = Number(document.querySelector(".guess").value);
-    if (!guess) {
-        console.log("Número inválido!");
+    if (!guess || guess > numMax) {
+        document.querySelector(".message").textContent = "Número inválido!";
+    } else if (guess === secretNumber) {
+        document.querySelector(".number").textContent = secretNumber;
+        document.querySelector(".message").textContent = "🎉 Parabéns! Número exato!";
+        document.querySelector("body").classList.add("success");
     } else {
-        console.log(guess);
-        document.querySelector(".guess").value = "";
+        if (score > 1) {
+            document.querySelector(".message").textContent = `${guess > secretNumber ? "📈 Muito alto!" : "📉 Muito baixo!"}`;
+            document.querySelector(".guess").value = "";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "💥 Você perdeu o jogo!";
+            document.querySelector("body").classList.add("error");
+        }
     }
 }
 
-// Ao clicar no botão "Check" ele chama a função verificar o número
+// Ao clicar no botão "New Game" chama a função newGame()
+document.querySelector(".new").addEventListener("click", newGame);
+
+// Ao clicar no botão "Check" chama a função checkNumber()
 document.querySelector(".check").addEventListener("click", checkNumber);
